82511f2498db291ac4b258da473a917029d6d567
Jay <bbvishsc1013@gmail.com>
2026-04-16 19:07:14 +0100

    configure static export for github pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  basePath: '/2026_DAC_Website',
+  assetPrefix: '/2026_DAC_Website/',
+  images: { unoptimized: true },
+}
 
 module.exports = nextConfig
